051b5d9b27de21505b8915c6c7898af51f167237
Ruben Marcus <ruben@rubenmarcus.dev>
2021-06-20 18:07:32 -0300

    Refactor

--- config/md-loader.js
+++ config/md-loader.js
@@ -1,11 +1,11 @@
-import Hooks from '../documentation/HOOKS.md'
-import Contribua from '../documentation/CONTRIBUA.md'
-import Links from '../documentation/LINKS.md'
+import Hooks from "../documentation/HOOKS.md";
+import Contribua from "../documentation/CONTRIBUA.md";
+import Links from "../documentation/LINKS.md";
 
 const MdLoader = {
-    hooks : Hooks,
-    contribua: Contribua,
-    links: Links
-}
+  hooks: Hooks,
+  contribua: Contribua,
+  links: Links,
+};
 
 export default MdLoader;
--- config/static-paths.js
+++ config/static-paths.js
@@ -1,9 +1,10 @@
 const ROUTES = [
-    {params: {slug: 'hooks' , title: 'Guia de Hooks'}},
-    {params: {slug: 'contribua' , title: 'Guia de Padrões React'}},
-    {params: {slug: 'links' , title: 'Guia de Padrões React'}},
+  { params: { slug: "hooks", title: "Guia de Hooks" } },
+  { params: { slug: "contribua", title: "Guia de Padrões React" } },
+  { params: { slug: "links", title: "Guia de Padrões React" } },
 ];
 
-export const STATIC_PATHS = {paths: ROUTES, fallback: false};
+export const STATIC_PATHS = { paths: ROUTES, fallback: false };
 
- export const PATH_PROPS = slug => STATIC_PATHS.paths.map(routes => routes.params).find( route => route.slug ===  slug );
+export const PATH_PROPS = (slug) =>
+  STATIC_PATHS.paths.filter(({ params }) => params.slug === slug)[0].params;
