f773e5ff16096b9dc5941fc8e524c4e5038dafe3
rubenmarcus <ruben@rubenmarcus.dev>
2021-07-11 06:57:41 -0300

    Doc files

--- config/md-loader.js
+++ config/md-loader.js
@@ -1,6 +1,6 @@
-import Hooks from "../documentation/hooks.md";
-import Contribua from "../documentation/CONTRIBUA.md";
-import Links from "../documentation/LINKS.md";
+import Hooks from "../documentation/DOC-HOOKS.md";
+import Contribua from "../documentation/DOC-CONTRIBUA.md";
+import Links from "../documentation/DOC-LINKS.md";
 
 const MdLoader = {
   hooks: Hooks,
